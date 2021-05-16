8ac12db063495cbc6b09ba4a15f81752f8ac37bf
MarceloBorges <melombm@hotmail.com>
2021-05-16 16:26:48 -0500

    atualizado

---
+++ ex-26.1/my-scripts/index.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync');
+
+const scripts = [
+  { name: 'Calcular IMC', script: './imc.js' },
+  { name: 'Calcular velocidade média', script './velocidade.js'},
+  { name: 'Jogo de adivinhação', script './sorteio.js'},
+];
---
+++ ex-26.1/my-scripts/velocidade.js
@@ -0,0 +1,8 @@
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Distância percorrida (m)');
+const tempo = readline.questionInt('Tempo gasto (s)');
+
+const velocidadeMedia = (distancia / tempo).toFixed(2);
+
+console.log(`Velocidade média: ${velocidadeMedia} m/s`);
