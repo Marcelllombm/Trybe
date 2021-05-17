49608d7ec7fafade7be8315ac760030013048e29
MarceloBorges <melombm@hotmail.com>
2021-05-17 15:50:05 -0500

    atualizado tudo

---
+++ ex-25.2/README.md
@@ -0,0 +1,3 @@
+Agora, a prática
+O MongoDb possui diversas ferramentas, como, por exemplo, mongo , mongosh , Compass e outras ferramentas de terceiros. Você pode utilizar o que achar melhor para executar as queries , o importante é realizá-las.
+Você continuará utilizando o banco de dados erp do dia anterior. Nos exercícios 1 a 8 , você utilizará o mesmo pipeline . A ideia é começar com um pipeline pequeno e ir adicionando estágios à medida que você for evoluindo nos exercícios. Vamos lá?
--- ex-26.1/my-scripts/index.js
+++ ex-26.1/my-scripts/index.js
@@ -1,7 +1,27 @@
 const readline = require('readline-sync');
 
-const scripts = [
-  { name: 'Calcular IMC', script: './imc.js' },
-  { name: 'Calcular velocidade média', script './velocidade.js'},
-  { name: 'Jogo de adivinhação', script './sorteio.js'},
-];
+function realizaFatoracao(x) {
+  // Se x for 0 ou 1
+  return [0, 1].includes(x)
+    // Retornamos 1
+    ? 1
+    // Caso contrário, continuamos o cálculo do fatorial multiplicando x pelo fatorial de x - 1
+    : x * realizaFatoracao(x - 1);
+}
+
+function realizaCalculo() {
+  const x = readline.questionInt('Informe o valor de x:');
+
+  if (x <= 0) {
+    console.log('Digite um número maior que 0!')
+    return;
+  }
+
+  console.log(`x: ${x}`);
+
+  const resultado = realizaFatoracao(x);
+
+  console.log(`Resultado: ${resultado}`);
+}
+
+realizaCalculo();
