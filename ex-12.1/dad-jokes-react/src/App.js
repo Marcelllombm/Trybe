d15f8f7376c47b34a43c008f018af0db8fd0f437
marcelllombm <melombm@hotmail.com>
2022-01-05 15:56:00 -0300

    exercicios 12.1

---
+++ ex-12.1/dad-jokes-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+// import Counter from './Counter';
+import DadJoke from './DadJoke';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <DadJoke />
+        {/* <Counter /> */}
+      </div>
+    )
+  }
+}
